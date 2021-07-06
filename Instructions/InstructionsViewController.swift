e129d21d4a9e2796d4d5e87afb8f28d08493a779
seoyeon <hongssup0202@gmail.com>
2021-07-06 09:24:24 +0900

    Basic settings

---
+++ Instructions/InstructionsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  InstructionsViewController.swift
+//  Instructions
+//
+//  Created by SeoYeon Hong on 2021/07/05.
+//
+
+import UIKit
+
+class InstructionsViewController: UIViewController {
+
+    
+    @IBOutlet weak var helpBtn: UIButton!
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
